fe6d5e8851ee4577e12159fb64dd39974b826b9b
Steven Echeverria <stevennava749@gmail.com>
2025-11-26 15:11:12 -0700

    Update CORS_ORIGINS with new Vercel URL

--- backend/app/config.py
+++ backend/app/config.py
@@ -19,7 +19,7 @@ class Settings:
         origin.strip() 
         for origin in os.getenv(
             "CORS_ORIGINS", 
-            "http://localhost:3000,http://localhost:5173,https://learningapp-iear.onrender.com/"
+            "https://learning-5kpyjg1hl-su1kiis-projects.vercel.app/"
         ).split(",")
     ]
 
