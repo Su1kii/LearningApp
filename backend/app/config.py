a601403937ae5954bbcb972c0cc10c0aeac92aaa
Steven Echeverria <stevennava749@gmail.com>
2025-11-26 14:55:41 -0700

    Update CORS_ORIGINS for production environment

--- backend/app/config.py
+++ backend/app/config.py
@@ -19,7 +19,7 @@ class Settings:
         origin.strip() 
         for origin in os.getenv(
             "CORS_ORIGINS", 
-            "http://localhost:3000,http://localhost:5173,https://learning-1bbze2ktk-su1kiis-projects.vercel.app"
+            "http://localhost:3000,http://localhost:5173,https://learningapp-iear.onrender.com/"
         ).split(",")
     ]
 
